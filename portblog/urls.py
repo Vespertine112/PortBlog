67051f333245747ac8ef53102b960ae2d213fc0a
Brayden Hill <hillbgh@aggiemail.usu.edu>
2022-02-13 01:45:07 -0700

    inital Commit

---
+++ portblog/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+
+app_name = 'portblog'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('resume/', views.resume, name='resume'),
+]
---
+++ portblog/views.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'portblog/index.html', context=None)
+
+def resume(request):
+    return render(request, 'portblog/resume.html', context=None)
